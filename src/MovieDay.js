8650cc8bb1c912a8a1f4f7b0380207cb721afc1d
Andrew Thomas Carroll <athomascarroll@gmail.com>
2023-06-05 15:36:17 -0700

    image path

--- src/MovieDay.js
+++ src/MovieDay.js
@@ -9,7 +9,7 @@ class MovieDay extends React.Component {
               <Card.Text>Overview: {this.props.movieData.overview}</Card.Text>
               <Card.Text>Average # of Votes: {this.props.movieData.averageVotes}</Card.Text>
               <Card.Text>Total # of Votes: {this.props.movieData.totalVotes}</Card.Text>
-              <Card.Img variant='top' src={this.props.movieData.image_url} />
+              <Card.Img variant='top' src={`https://image.tmdb.org/t/p/w500${this.props.movieData.image_url}`} />
               <Card.Text>Popularity Level: {this.props.movieData.popularity}</Card.Text>
               <Card.Text>Release Date: {this.props.movieData.releaseDate}</Card.Text>
             </Card>
