3566542394d1f51a689ca4b603cc94ed5bfefc53
Andrew Thomas Carroll <athomascarroll@gmail.com>
2023-06-05 15:19:34 -0700

    cleaned up

--- src/MovieDay.js
+++ src/MovieDay.js
@@ -4,29 +4,15 @@ import Card from 'react-bootstrap/Card'
 class MovieDay extends React.Component {
   render() {
     return (
-      <>
-        {this.props.movieData ? (
-          this.props.movieData.map((movie, idx) =>
-            <Card key={idx} className='movieDay'>
-              <Card.Text>Title: {movie.title}</Card.Text>
-              <Card.Text>Overview: {movie.overview}</Card.Text>
-              <Card.Text>Average # of Votes: {movie.averageVotes}</Card.Text>
-              <Card.Text>Total # of Votes: {movie.totalVotes}</Card.Text>
-              {movie.image_url
-                ?
-                <Card.Img
-                  src={`https://image.tmdb.org/t/p/w500${movie.image_url}`}
-                /> : ''
-              }
-              <Card.Text>Popularity Level: {movie.popularity}</Card.Text>
-              <Card.Text>Release Date: {movie.releaseDate}</Card.Text>
+        <Card className='movieDay'>
+              <Card.Text>Title: {this.props.movieData.title}</Card.Text>
+              <Card.Text>Overview: {this.props.movieData.overview}</Card.Text>
+              <Card.Text>Average # of Votes: {this.props.movieData.averageVotes}</Card.Text>
+              <Card.Text>Total # of Votes: {this.props.movieData.totalVotes}</Card.Text>
+              <Card.Img variant='top' src={this.props.movieData.image_url} />
+              <Card.Text>Popularity Level: {this.props.movieData.popularity}</Card.Text>
+              <Card.Text>Release Date: {this.props.movieData.releaseDate}</Card.Text>
             </Card>
-          )
-        ) : (
-          <p>loading data; </p>
-        )
-        }
-      </>
     );
   }
 }
